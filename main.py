854d5639b568d74ce22a1190fe98a48408121fbd
Philip Bretz <pbretz99@gmail.com>
2025-01-25 12:16:38 -0600

    downloader

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from downloader.fiction import Fiction, FictionRequest
-from downloader.fetcher import ChapterFetcher
-from downloader.renderer import HTMLRenderer
+from downloader.fiction import Fiction
+from downloader.download import download as download_core
+from downloader.renderer import RenderFormat
 from downloader.mock_database import MockDatabase
 from fastapi import FastAPI
 from typing import Optional
@@ -13,29 +13,19 @@ app = FastAPI(title="Royal Road Fiction Downloader")
 
 
 @app.get("/fiction/{title}")
-async def read_fiction(title: str):
+async def read_fiction(title: str) -> Fiction | None:
     return db.read(title)
 
 @app.get("/fictions/")
-async def read_fictions(skip: int = 0, limit: int = 10):
+async def read_fictions(skip: int = 0, limit: int = 10) -> list[Fiction]:
     return db.view(skip, limit)
 
-def _download(title: str, number: int, chapters: Optional[int]):
-    fiction_request = FictionRequest(title=title, number=number)
-    fetcher = ChapterFetcher(fiction_request)
-    fiction_details = fetcher.fetch_details()
-    db.create(fiction_details)
-    chapters = fetcher.fetch(up_to_chapter=chapters)
-    renderer = HTMLRenderer()
-    renderer.save_to_file(fiction_details, chapters)
-    return fiction_details
-
 @app.get("/download/{title}/{number}")
-async def download(title: str, number: int, chapters: Optional[int] = None):
-    return _download(title, number, chapters)
+async def download(title: str, number: int, chapters: Optional[int] = None, format: RenderFormat = RenderFormat.HTML) -> Fiction:
+    return download_core(title, number, chapters, format, db)
 
 @app.get("/download_from_title/{title}")
-async def download(title: str, chapters: Optional[int] = None):
+async def download(title: str, chapters: Optional[int] = None, format: RenderFormat = RenderFormat.HTML) -> Fiction | None:
     fiction = db.read(title)
     if fiction is not None:
-        return _download(title, fiction.number, chapters)
+        return download_core(title, fiction.number, chapters, format, db)
